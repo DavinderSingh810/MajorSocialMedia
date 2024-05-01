7344598334c0c5bffa85a4ee5b29afc57815bbfb
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2024-05-02 01:32:23 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import express from "express"
 import cookieParser from "cookie-parser"  
 import userRoutes from "./routes/userRoutes.js"
+import postRoutes from "./routes/postRoutes.js"
 
 
 dotenv.config();
@@ -17,7 +18,7 @@ app.use(cookieParser())
 
 //Routes
 app.use("/api/users", userRoutes);
-// app.use("/api/posts", postRoutes);
+app.use("/api/posts", postRoutes);
 // app.use("/api/messages", messageRoutes);
   
 
