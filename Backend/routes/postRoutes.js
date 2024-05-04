522910e376437486c561937b6fa2b8e17984bb72
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2024-05-05 02:10:35 +0530

    Update

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,10 +1,15 @@
 import express from "express"
-import  {createPost}  from "../controllers/postController.js"
+import  {createPost,getPost,deletePost, likeUnlikePost, replyToPost , getFeedPosts}  from "../controllers/postController.js"
+import protectRoute from "../middlewares/protectRoute.js"
 
 
 const router = express.Router()
-
-router.post("/create",createPost)
+router.get("/:id",getPost)
+router.get("/feed",protectRoute,getFeedPosts)
+router.delete("/:id",protectRoute,deletePost)
+router.post("/create",protectRoute,createPost)
+router.post("/like/:id",protectRoute,likeUnlikePost)
+router.post("/reply/:id",protectRoute,replyToPost)
 
 
 export default router
--- Fontend/src/App.jsx
+++ Fontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Container } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom"
 import UserPage from "./pages/UserPage"
 import PostPage from "./pages/PostPage"
+import AuthPage from "./pages/AuthPage"
 import Header from "./compenents/Header"
 function App() {
   
@@ -10,6 +11,9 @@ function App() {
   <Container maxW="620px">
     <Header />
   <Routes>
+   <Route path="/" element={<HomePage/>}/>
+   <Route path="/auth" element={<AuthPage/>}/>
+
      < Route path="/:username" element={<UserPage />}></Route>
      <Route path="/:username/Post/:pid" element={<PostPage />}></Route>
   </Routes>
